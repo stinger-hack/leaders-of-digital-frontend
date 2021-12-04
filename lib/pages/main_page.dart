07d67adc466005c1e3579942105f86e4a6510dc7
Dragonite24 <qwez-9@mail.ru>
2021-12-04 07:21:57 +0300

    controller fix

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -17,173 +17,130 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
   TabController? tabController;
   int _index = 0;
 
+  bool isNotification = false;
+
   @override
   void initState() {
-    tabController = TabController(length: widget.isAdmin ? 6 : 5, vsync: this);
+    tabController = TabController(length: widget.isAdmin ? 5 : 4, vsync: this);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Stack(
-      children: [
-        Image.asset(
-            "images/backAuth.png",
-          fit: BoxFit.cover,
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width
-        ),
+          body: Stack(children: [
+        Image.asset("images/backAuth.png",
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width),
         SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.only(
-                      top: 38,
-                      left: 110,
-                      right: 94,
-                      bottom: 46
-                  ),
-                  child: Row(
-                      children: [
-                        Image.asset(
-                            "images/fund.png",
-                            height: 50,
-                            width: 146
-                        ),
-                        const SizedBox(width: 169),
-                        Expanded(
-                            child: TabBar(
-                                controller: tabController,
-                                physics: const NeverScrollableScrollPhysics(),
-                                indicatorColor: const Color(0xff009A96),
-                                isScrollable: true,
-                                onTap: (index) => _index = index,
-                                tabs: [
-                                  tabBarTab("Проекты"),
-                                  tabBarTab("Новости"),
-                                  tabBarTab("Избранное"),
-                                  if (widget.isAdmin) tabBarTab("Мои проекты"),
-                                  tabBarTab("События"),
-                                  Container()
-                                ]
-                            )
-                        ),
-                        GestureDetector(
-                            onTap: () => setState(() {
-                              tabController!.animateTo(
-                                  6,
-                                  duration: Duration(milliseconds: 500),
-                                  curve: Curves.ease
-                              );
-                              _index = 6;
-                            }),
-                            child: Container(
-                                height: 46,
-                                width: 60,
-                                color: Colors.transparent,
-                                child: Stack(
-                                    children: [
-                                      Positioned(
-                                          left: 0,
-                                          bottom: 0,
-                                          child: Container(
-                                              height: 44,
-                                              width: 44,
-                                              alignment: Alignment.center,
-                                              decoration: BoxDecoration(
-                                                  color: _index == 6
-                                                      ? const Color(0xff009A96)
-                                                      : Colors.white,
-                                                  borderRadius: BorderRadius.circular(22),
-                                                  boxShadow: const [BoxShadow(
-                                                      offset: Offset(0, 2),
-                                                      blurRadius: 10,
-                                                      color: Color.fromRGBO(0, 0, 0, 0.05)
-                                                  )]
-                                              ),
-                                              child: Image.asset(
-                                                  "images/bell.png",
-                                                  color: _index == 6
-                                                      ? Colors.white
-                                                      : const Color(0xff25222C),
-                                                  height: 20,
-                                                  width: 18
-                                              )
-                                          )
-                                      ),
-                                      Positioned(
-                                          top: 0,
-                                          right: 0,
-                                          child: Container(
-                                              height: 24,
-                                              width: 40,
-                                              alignment: Alignment.center,
-                                              decoration: BoxDecoration(
-                                                  color: _index == 6
-                                                      ? Colors.white
-                                                      : const Color(0xff009A96),
-                                                  borderRadius: BorderRadius.circular(16),
-                                                  boxShadow: const [BoxShadow(
-                                                      offset: Offset(0, 4),
-                                                      blurRadius: 4,
-                                                      color: Color.fromRGBO(0, 0, 0, 0.05)
-                                                  )]
-                                              ),
-                                              child: Text(
-                                                  "7",
-                                                  style: TextStyle(
-                                                      fontWeight: FontWeight.w600,
-                                                      fontSize: 16,
-                                                      color: _index == 6
-                                                          ? const Color(0xff009A96)
-                                                          : Colors.white
-                                                  )
-                                              )
-                                          )
-                                      )
-                                    ]
-                                )
-                            )
-                        )
-                      ]
-                  )
-              ),
-              SizedBox(
-                  height: MediaQuery.of(context).size.height - 136,
-                  child: TabBarView(
-                      controller: tabController,
-                      children: [
-                        const ShowcasePage(),
-                        const NewsPage(),
-                        const FavoritesPage(),
-                        if (widget.isAdmin) const MyProjectsPage(),
-                        const GoogleMap(),
-                        NotificationsPage()
-            ]
-                  )
-              )
-      ]
-          )
-        )
-      ]
-    )
-  );
+            child: Column(children: [
+          Padding(
+              padding: const EdgeInsets.only(
+                  top: 38, left: 110, right: 94, bottom: 46),
+              child: Row(children: [
+                Image.asset("images/fund.png", height: 50, width: 146),
+                const SizedBox(width: 169),
+                Expanded(
+                    child: TabBar(
+                        controller: tabController,
+                        physics: const NeverScrollableScrollPhysics(),
+                        indicatorColor: const Color(0xff009A96),
+                        isScrollable: true,
+                        onTap: (index) => _index = index,
+                        tabs: [
+                      tabBarTab("Проекты"),
+                      tabBarTab("Новости"),
+                      tabBarTab("Избранное"),
+                      if (widget.isAdmin) tabBarTab("Мои проекты"),
+                      tabBarTab("События")
+                    ])),
+                GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isNotification = !isNotification;
+                      });
+                    },
+                    child: Container(
+                        height: 46,
+                        width: 60,
+                        color: Colors.transparent,
+                        child: Stack(children: [
+                          Positioned(
+                              left: 0,
+                              bottom: 0,
+                              child: Container(
+                                  height: 44,
+                                  width: 44,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: isNotification
+                                          ? const Color(0xff009A96)
+                                          : Colors.white,
+                                      borderRadius: BorderRadius.circular(22),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            offset: Offset(0, 2),
+                                            blurRadius: 10,
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.05))
+                                      ]),
+                                  child: Image.asset("images/bell.png",
+                                      color: isNotification
+                                          ? Colors.white
+                                          : const Color(0xff25222C),
+                                      height: 20,
+                                      width: 18))),
+                          Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Container(
+                                  height: 24,
+                                  width: 40,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: isNotification
+                                          ? Colors.white
+                                          : const Color(0xff009A96),
+                                      borderRadius: BorderRadius.circular(16),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            offset: Offset(0, 4),
+                                            blurRadius: 4,
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.05))
+                                      ]),
+                                  child: Text("7",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 16,
+                                          color: isNotification
+                                              ? const Color(0xff009A96)
+                                              : Colors.white))))
+                        ])))
+              ])),
+          SizedBox(
+              height: MediaQuery.of(context).size.height - 136,
+              child: TabBarView(controller: tabController, children: [
+                const ShowcasePage(),
+                const NewsPage(),
+                const FavoritesPage(),
+                if (widget.isAdmin) const MyProjectsPage(),
+                isNotification ? NotificationsPage() : const GoogleMap()
+              ]))
+        ]))
+      ]));
 
   Container tabBarTab(String text) => Container(
-    width: 114,
-    child: Tab(
-        child: Text(
-            text,
-            style: const TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-                color: Color(0xff25222C)
-            )
-        )
-    )
-  );
+      width: 114,
+      child: Tab(
+          child: Text(text,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  color: Color(0xff25222C)))));
 }
--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:stinger_web/components/my_button.dart';
 import 'package:stinger_web/models/notification_model.dart';
 
 class NotificationsPage extends StatelessWidget {
@@ -10,125 +12,101 @@ class NotificationsPage extends StatelessWidget {
         name: "Ксения Ливина",
         date: "12 октября",
         header: "Traffic Data: узнать все о транспортных потоках",
-        body: "Лучшего решения проблемы я еще не видела!"
-    ),
+        body: "Лучшего решения проблемы я еще не видела!"),
     NotificationModel(
         header: "Traffic Data: узнать все о транспортных потоках",
-        body: "Приглашает на событие."
-    ),
+        body: "Приглашает на событие."),
     NotificationModel(
         header: "Acerta: предоставление оборудования",
-        body: "Запрос на встречу с командой."
-    ),
+        body: "Запрос на встречу с командой."),
     NotificationModel(
         header: "Acerta: предоставление оборудования",
-        body: "Ура! Прототип проекта готов!"
-    ),
+        body: "Ура! Прототип проекта готов!"),
     NotificationModel(
         url: "images/avatar5.png",
         name: "Николай Кузин",
         date: "1 октября",
         header: "Acerta: предоставление оборудования",
-        body: "Ребята молодцы! Всегда все на высшем уровне!!!)"
-    )
+        body: "Ребята молодцы! Всегда все на высшем уровне!!!)")
   ];
 
   @override
-  Widget build(BuildContext context) => Wrap(
-      spacing: 40,
-      runSpacing: 40,
-      direction: Axis.vertical,
-      children: [
-        for (int i = 0; i < notificationModels.length; i++)
-          notification(i)
-      ]
-  );
+  Widget build(BuildContext context) => Padding(
+      padding: const EdgeInsets.all(50),
+      child: AnimationLimiter(child: Wrap(
+          spacing: 40,
+          runSpacing: 40,
+          direction: Axis.vertical,
+          children: [
+            for (int i = 0; i < notificationModels.length; i++)
+              AnimationConfiguration.staggeredGrid(
+                  columnCount: 2,
+                  position: i,
+                  duration: const Duration(milliseconds: 375),
+                  child: ScaleAnimation(
+                      scale: 0.5,
+                      child: FadeInAnimation(
+                          child: notification(i)
+                      )
+                  )
+              )
+          ])));
 
   Container notification(int index) => Container(
-    height: 150,
-    width: 590,
-      padding: const EdgeInsets.only(
-          top: 16,
-          left: 20,
-          right: 20,
-          bottom: 13
-      ),
+      height: 150,
+      width: 590,
+      padding: const EdgeInsets.only(top: 16, left: 20, right: 20, bottom: 13),
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(16),
-          boxShadow: const [BoxShadow(
-              offset: Offset(0, 2),
-              blurRadius: 10,
-              color: Color.fromRGBO(0, 0, 0, 0.05)
-          )]
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (notificationModels[index].url != null)
-              Row(
-                  children: [
-                    CircleAvatar(
-                        child: Image.asset(notificationModels[index].url!)
-                    ),
-                    const SizedBox(width: 10),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              notificationModels[index].name!,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                  color: Color(0xff25222C)
-                              )
-                          ),
-                          Text(
-                              notificationModels[index].date!,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 14,
-                                  color: Color(0xffB0AFB3)
-                              )
-                          )
-                        ]
-                    )
-                  ]
-              ),
-            const SizedBox(height: 12),
-            Text(
-                notificationModels[index].header!,
-                style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Color(0xff25222C)
-                )
-            ),
-            const SizedBox(height: 8),
-            Row(
-                children: [
-                  Text(
-                    notificationModels[index].body!,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                        color: Color(0xff66646B)
-                    ),
-                  ),
-                  const Spacer(),
-                  Container(
-                      height: 32,
-                      width: 32,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: const Color(0xff009A96),
-                          borderRadius: BorderRadius.circular(16)
-                      ),
-                      child: const Icon(Icons.arrow_forward_ios)
-                  )
-                ]
-            )
-          ]
-      )
-  );
+          boxShadow: const [
+            BoxShadow(
+                offset: Offset(0, 2),
+                blurRadius: 10,
+                color: Color.fromRGBO(0, 0, 0, 0.05))
+          ]),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        if (notificationModels[index].url != null)
+          Row(children: [
+            CircleAvatar(child: Image.asset(notificationModels[index].url!)),
+            const SizedBox(width: 10),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(notificationModels[index].name!,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: Color(0xff25222C))),
+              Text(notificationModels[index].date!,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Color(0xffB0AFB3)))
+            ])
+          ]),
+        const SizedBox(height: 12),
+        Text(notificationModels[index].header!,
+            style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+                color: Color(0xff25222C))),
+        const SizedBox(height: 8),
+        Row(children: [
+          Text(
+            notificationModels[index].body!,
+            style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                color: Color(0xff66646B)),
+          ),
+          const Spacer(),
+          Container(
+              height: 32,
+              width: 32,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: const Color(0xff009A96),
+                  borderRadius: BorderRadius.circular(16)),
+              child: const Icon(Icons.arrow_forward_ios))
+        ])
+      ]));
 }
